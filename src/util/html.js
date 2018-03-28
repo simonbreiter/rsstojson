b16f9f577fd7f476e6ca1feced47b5befc932a4d
Simon Breiter <hello@simonbreiter.com>
2018-03-28 10:41:36 +0200

    feat: add web analytics

--- src/util/html.js
+++ src/util/html.js
@@ -26,6 +26,21 @@ const html = (css, app) => `
         <link rel="icon" type="image/png" href="/img/favicon-128.png" sizes="128x128" />
         <title>RSS to JSON</title>
         ${css}
+        <!-- Matomo -->
+        <script type="text/javascript">
+          var _paq = _paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//analytics.simonbreiter.com/";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <!-- End Matomo Code -->
     </head>
     <body>
         <div id="app">${app}</div>
