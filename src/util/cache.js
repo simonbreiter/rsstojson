cb6a1ad795462def4a706788835e0e2540c89b5f
Simon Breiter <hello@simonbreiter.com>
2018-03-28 18:25:18 +0200

    refactor: remove log messages

--- src/util/cache.js
+++ src/util/cache.js
@@ -5,11 +5,9 @@ const cache = duration => {
     const key = `__express__${req.originalUrl || req.url}`
     const cachedBody = mcache.get(key)
     if (cachedBody) {
-      console.log('Send cached body')
       res.send(cachedBody)
       return
     } else {
-      console.log('Send live version and save body in memory')
       res.sendResponse = res.send
       res.send = body => {
         mcache.put(key, body, duration)
